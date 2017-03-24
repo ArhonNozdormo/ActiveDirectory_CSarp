9b850ba82f25bacc25770be700d3f07045b1ca16
Arhon <Arhon@ARHON-PC>
2017-03-25 00:09:59 +0300

    Modifier project

---
+++ WorkWithAD/AD.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.DirectoryServices.AccountManagement;
+
+namespace WorkWithAD
+{
+    //ActiveDirectory
+    class AD
+    {
+        string currentDomain;
+        string username;
+        string password;
+        ContextType type;
+
+        // Для работы с Active Directory
+        public AD(string user, string pass, string domain)
+        {
+            this.currentDomain = domain;
+            this.type = ContextType.Domain;
+            this.username = user;
+            this.password = pass;
+        }
+
+        // Для работы с local users
+        public AD()
+        {
+            this.currentDomain = Environment.MachineName;
+            this.type = ContextType.Machine;
+            this.username = null;
+            this.password = null;
+        }
+
+        //поиск пользователя в домене AD
+        public bool searchUser(string ADUsername)
+        {
+            try
+            {
+                using (PrincipalContext contex = new PrincipalContext(
+                    this.type,
+                    this.currentDomain,
+                    this.username,
+                    this.password)
+                    )
+                {
+                    UserPrincipal user = UserPrincipal.FindByIdentity(
+                        contex,
+                        IdentityType.SamAccountName,
+                        ADUsername
+                        );
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return false;
+        }
+
+        ~AD()
+        {
+            Console.WriteLine("Destroy CurrentDomain");
+        }
+    }
+}
--- WorkWithAD/Program.cs
+++ WorkWithAD/Program.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.DirectoryServices;
-using System.DirectoryServices.AccountManagement;
-using System.Collections;
 
 namespace WorkWithAD
 {
@@ -18,63 +11,6 @@ namespace WorkWithAD
             if (isEntry)
                 Console.WriteLine("Is entry");
             Console.Read();
-        }
-    }
-    //ActiveDirectory
-    class AD
-    {
-        string currentDomain;
-        string username;
-        string password;
-        ContextType type;
-
-        // Для работы с Active Directory
-        public AD(string user, string pass, string domain)
-        {
-            this.currentDomain = domain;
-            this.type = ContextType.Domain;
-            this.username = user;
-            this.password = pass;
-        }
-
-        // Для работы с local users
-        public AD()
-        {
-            this.currentDomain = Environment.MachineName;
-            this.type = ContextType.Machine;
-            this.username = null;
-            this.password = null;
-        }
-        //поиск пользователя в домене AD
-        public bool searchUser(string ADUsername)
-        {
-            try
-            {
-                using (PrincipalContext contex = new PrincipalContext(
-                    this.type,
-                    this.currentDomain,
-                    this.username,
-                    this.password)
-                    )
-                {
-                    UserPrincipal user = UserPrincipal.FindByIdentity(
-                        contex, 
-                        IdentityType.SamAccountName, 
-                        ADUsername
-                        );
-                    return true;
-                }
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            return false;
-        }
-
-        ~AD()
-        {
-            Console.WriteLine("Destroy CurrentDomain");
         }
     }
 }
