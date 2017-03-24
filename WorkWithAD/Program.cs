43d2b06b2027ecc10090ee1e238950e66f98b8f4
Arhon <Arhon@ARHON-PC>
2017-03-24 21:43:00 +0300

    Create AD class

--- WorkWithAD/Program.cs
+++ WorkWithAD/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.DirectoryServices;
+using System.Collections;
 
 namespace WorkWithAD
 {
@@ -10,6 +12,39 @@ namespace WorkWithAD
     {
         static void Main(string[] args)
         {
+            try
+            {
+                DirectoryEntry CurrentDomain = new DirectoryEntry();
+                Console.WriteLine(CurrentDomain.Name);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+    //ActiveDirectory
+    class AD
+    {
+        DirectoryEntry CurrentDomain;
+        DirectoryEntries Tree;
+        IEnumerator Reed;
+
+        public AD(string path, string username, string password)
+        {
+            this.CurrentDomain = new DirectoryEntry(path, username, password);
+            this.Tree = CurrentDomain.Children;
+            this.Reed = Tree.GetEnumerator();
+        }
+        public AD()
+        {
+            this.CurrentDomain = new DirectoryEntry();
+            this.Tree = CurrentDomain.Children;
+            this.Reed = Tree.GetEnumerator();
         }
     }
 }
